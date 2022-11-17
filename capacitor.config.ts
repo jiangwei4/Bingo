81809f904f02f2fc98c8864ea49f8273bf15cf69
donovan <dono.rock@hotmail.fr>
2022-11-17 18:48:59 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bingo15V2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
